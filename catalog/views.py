8098fce05aed081a3b1ec8033e9cfaec4f7b6d17
MaksimVolkov <skazimko@yandex.ru>
2024-11-15 12:13:34 +0300

    add produtc_detail.html

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from catalog.models import Product
 
 
@@ -18,3 +18,9 @@ def product_list(request, pk):
     products = Product.objects.all()
     context = {'products': products}
     return render(request, 'product/product_list.html', context=context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, id=pk)
+    context = {'product': product}
+    return render(request, 'product_detail.html', context=context)
