570cc3647ca7535f0f64ad5165a6654ed634fac5
MaksimVolkov <skazimko@yandex.ru>
2024-11-15 11:38:59 +0300

    init

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
 from catalog import views
+from catalog.views import index
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", views.home, name="index"),
     path("contacts/", views.contacts, name="contacts"),
+    path('', index),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from catalog.models import Product
 
 
 def home(request):
@@ -7,3 +8,13 @@ def home(request):
 
 def contacts(request):
     return render(request, "contacts.html")
+
+
+def index(request):
+    return render(request, 'base.html')
+
+
+def product_list(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'base.html', context)
