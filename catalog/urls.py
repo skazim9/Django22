9c3c42c332d89021d7d40d40eb49c2293e6d6d07
MaksimVolkov <skazimko@yandex.ru>
2024-11-15 12:39:58 +0300

    update

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,8 +5,8 @@ from catalog.views import contacts, product_detail, home, product_list
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", home, name="index"),
-    path("contacts/", contacts, name="contacts"),
+    path('', home, name='home'),
+    path('contacts/', contacts, name="contacts"),
     path('product_list/', product_list, name='product_list'),
     path('product_detail/<int:pk>/', product_detail, name='product_detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,17 +1,20 @@
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from catalog.models import Product
 
 
 def home(request):
-    return render(request, "home.html")
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'base.html', context=context)
 
 
 def contacts(request):
-    return render(request, "contacts.html")
-
-
-def index(request):
-    return render(request, 'base.html')
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        message = request.POST.get('message')
+        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
+    return render(request, 'contacts.html')
 
 
 def product_list(request, pk):
