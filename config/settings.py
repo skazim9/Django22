81cda29fcf719956a23703fc027b01a9e0245e73
MaksimVolkov <skazimko@yandex.ru>
2024-12-12 20:43:37 +0300

    update login, logout

--- config/settings.py
+++ config/settings.py
@@ -136,3 +136,9 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = "users.User"
+
+LOGIN_REDIRECT_URL = "/"
+
+LOGOUT_REDIRECT_URL = "/"
+
+LOGIN_URL = "users:login"
--- users/urls.py
+++ users/urls.py
@@ -24,4 +24,5 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
