31a184f4fe3952cd0f4e87820838af85d210a59b
MaksimVolkov <skazimko@yandex.ru>
2024-12-12 21:04:52 +0300

    update register and templates

--- users/urls.py
+++ users/urls.py
@@ -25,4 +25,5 @@ app_name = UsersConfig.name
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
