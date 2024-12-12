cbffd24390f0c05384ae1db123b5a444556d3661
MaksimVolkov <skazimko@yandex.ru>
2024-12-12 20:17:24 +0300

    create login

---
+++ users/forms.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.forms import UserCreationForm
+from users.models import User
+
+
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('email', 'password1', 'password2')
